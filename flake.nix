e8e1f033774eb29b764a52c8d642e20f270785e2
Jade <jade@witchof.space>
2022-11-16 15:37:56 -0500

    Fixed embarassing flake mistake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   description = "Jade's Nix templates";
 
-  templates = {
-    rust = {
-      path = ./rust;
-      description = "Rust project with naersk and rust-overlay";
-      welcomeText = ''
-        Hewwo! It's time for UwUst! :3
-      '';
+  outputs = { self }: {
+    templates = {
+      rust = {
+        path = ./rust;
+        description = "Rust project with naersk and rust-overlay";
+        welcomeText = ''
+          Hewwo! It's time for UwUst! :3
+        '';
+      };
     };
   };
 }
