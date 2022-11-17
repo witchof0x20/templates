78ff1bcb97db098faf36050da6efd38e466aada4
Jade <jade@witchof.space>
2022-11-16 21:31:18 -0500

    Added necessary components to rust

--- rust/flake.nix
+++ rust/flake.nix
@@ -7,13 +7,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
@@ -21,23 +17,16 @@
           overlays = [ (import rust-overlay) ];
         };
         rust_toolchain = pkgs.rust-bin.stable.latest;
-        naersk' = pkgs.callPackage naersk {
-          rustc = rust_toolchain.minimal;
-          cargo = rust_toolchain.minimal;
-        };
         nativeBuildInputs = [ pkgs.pkg-config ];
         buildInputs = [ ];
       in
       rec {
-        defaultPackage = naersk'.buildPackage {
-          inherit nativeBuildInputs buildInputs;
-          src = ./.;
-        };
-
         devShell = pkgs.mkShell {
           inherit buildInputs;
           nativeBuildInputs = nativeBuildInputs ++ [
-            rust_toolchain.default
+            (rust_toolchain.default.override {
+              extensions = [ "rust-src" "rustfmt" "rls" "clippy" ];
+            })
           ];
         };
       }
